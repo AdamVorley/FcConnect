e7faea544a595bb452ae3c854d58796b5cd75658
AdamVorley <adam_vorley@icloud.com>
2024-03-28 10:52:48 +0000

    T&C accepted user claim functionality added

--- FcConnect/Pages/Messaging/Messages.cshtml.cs
+++ FcConnect/Pages/Messaging/Messages.cshtml.cs
@@ -15,16 +15,20 @@ namespace FcConnect.Pages.Messaging
     {
         private readonly FcConnect.Data.ApplicationDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public IndexModel(FcConnect.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        public IndexModel(FcConnect.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
             _userManager = userManager;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IList<Message> Message { get;set; } = default!;
         public IList<Conversation> Conversation { get; set; }
         public string userId;
+        public string SvgContent { get; private set; }
+
 
         public async Task OnGetAsync()
         {
@@ -35,7 +39,12 @@ namespace FcConnect.Pages.Messaging
 
             Conversation = await _context.Conversation.Where(c => c.Users.Contains(user) && c.Messages.Any(m => m.Recipient == user)).OrderByDescending(c => c.LastMessageSent)
                 .Include(c => c.Messages).Include(c => c.Users).ToListAsync();
-      
+
+            if (Conversation.Count < 1) 
+            {            
+                var svgFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "Assets", "mail_1.svg");
+                SvgContent = System.IO.File.ReadAllText(svgFilePath);
+            }
 
             Message = await _context.Message.Where(m => m.Recipient == user).OrderByDescending(m => m.DateTimeSent).ToListAsync();
         }
--- FcConnect/Pages/Terms/Index.cshtml.cs
+++ FcConnect/Pages/Terms/Index.cshtml.cs
@@ -1,10 +1,33 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace FcConnect.Areas.Identity.Pages.Terms
 {
     public class IndexModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IndexModel(UserManager<IdentityUser> userManager) 
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> OnPostAgreeAsync()
+        {
+            // update user claim to terms accepted
+            var user = await _userManager.GetUserAsync(User);
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var existingClaim = userClaims.FirstOrDefault(r => r.Type == "TermsAccepted");
+
+            await _userManager.ReplaceClaimAsync(user, existingClaim, new Claim("TermsAccepted", "true"));
+
+            // TODO - log
+
+            return RedirectToPage("/Index");
+        }
+
         public void OnGet()
         {
         }

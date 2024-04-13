8b0badf42f6880aed7645d887894c1837f4fe5c9
AdamVorley <adam_vorley@icloud.com>
2024-04-13 11:15:21 +0100

    DateTime class bug fix

--- FcConnect/Utilities/GetDateTime.cs
+++ FcConnect/Utilities/GetDateTime.cs
@@ -2,11 +2,11 @@
 {
     public class GetDateTime
     {
-        static DateTime timeUtc = DateTime.UtcNow;
         static TimeZoneInfo gmtZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
 
         public static DateTime GetGMT() 
         {
+            DateTime timeUtc = DateTime.UtcNow;
             DateTime gmtDateTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, gmtZone);
             return gmtDateTime;
         }
